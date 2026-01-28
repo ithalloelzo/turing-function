81d9683cc630223c882561868f763c29d0c5f82e
ithalloelzo <ithalloelzo@gmail.com>
2026-01-28 13:05:48 -0300

    :tada: Commit inicial

---
+++ turing-function.js
@@ -0,0 +1,11 @@
+/*Usando o Javascript, desenvolva uma função que se baseie na máquina de Turing para conseguir realizar todas as operações matemáticas presente em um array e que retorne um array contendo os resultados.*/
+
+// Algoritmos permitidos: 
+/*+ Soma
+- Subtração
+/ Divisão
+* Multiplicação
+% Divisão com resto*/
+
+//[1, "+", 2] = 3
+//[1, "+", 2, 3, "*", 3]  = [3, 9]

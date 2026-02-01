9631a65fa31cfc51bcb749511c6b64036ad33434
ithalloelzo <ithalloelzo@gmail.com>
2026-02-01 15:26:16 -0300

    ✨ feat: implemente basic addition logic

--- turing-function.js
+++ turing-function.js
@@ -9,3 +9,29 @@
 
 //[1, "+", 2] = 3
 //[1, "+", 2, 3, "*", 3]  = [3, 9]
+
+function turingMachine(numeral_expression){
+  let result = []
+
+  for(let i = 0; i < numeral_expression.length; i+=3){
+
+    let num1 = numeral_expression[i]
+    let operator = numeral_expression[i+1]
+    let num2 = numeral_expression[i+2]
+    let calculation
+
+    switch (operator){
+      case "+":
+        calculation = num1 + num2
+      break;
+
+      default:
+        console.log("Error")
+    }
+    result.push(calculation)
+  }
+
+  return result
+}
+
+console.log(turingMachine([1,"+",2,4,"+",8]))
